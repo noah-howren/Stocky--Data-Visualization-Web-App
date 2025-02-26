56375eca38d15014835c6703ee35e7e30a97d1f5
Noah Howren <noah@howren.dev>
2025-02-25 22:22:06 -0800

    Update BACKEND variables

--- frontend/src/components/nav_bar/nav_bar.vue
+++ frontend/src/components/nav_bar/nav_bar.vue
@@ -126,7 +126,7 @@ export default{
             else{
                 this.pathAdd = "/stocks/"
             }
-            const path = 'http://127.0.0.1:5000/tickerList/' + this.exchange;
+            const path = import.meta.env.backend + '/tickerList/' + this.exchange;
             const res = await axios.get(path)
             this.tickerList = res.data['tickers']; 
             this.volumeList = res.data['volume'];
